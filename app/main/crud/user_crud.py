778bb7ab4b66f7476b5ee91c91fedd96cdf296f9
Laurent Alphonse ETOUDI BODO <laurentalphonsewilfried@gmail.com>
2024-06-06 17:33:55 +0200

    Edit Profile

--- app/main/crud/user_crud.py
+++ app/main/crud/user_crud.py
@@ -73,7 +73,7 @@ from typing import Optional
 
 class UserService:
     @classmethod
-    def update_profile(cls, db: Session, user_uuid: str, first_name: Optional[str] = None, last_name: Optional[str] = None, address: Optional[str] = None, phone_number: Optional[str] = None,email: Optional[str] = None,birthday: Optional[str] = None, avatar_file: Optional[FileUpload] = None):
+    def update_profile(cls, db: Session, user_uuid: str, first_name: Optional[str] = None, last_name: Optional[str] = None, email: Optional[str] = None,address: Optional[str] = None, phone_number: Optional[str] = None,birthday: Optional[str] = None, avatar_file: Optional[FileUpload] = None):
         user = db.query(User).filter(User.uuid == user_uuid).first()
         
         if not user:
@@ -83,12 +83,12 @@ class UserService:
             user.first_name = first_name
         if last_name is not None:
             user.last_name = last_name
+        if email is not None:
+            user.email = email
         if address is not None:
             user.address = address
         if phone_number is not None:
             user.phone_number = phone_number
-        if email is not None:
-            user.email = email
         if birthday is not None:
             user.birthday = birthday
         
