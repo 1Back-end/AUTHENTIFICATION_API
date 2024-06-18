6d9f31fa532fa342bdcff2f602d096ec4947a2e1
Severin Fernando Tanga Mvondo <jarodak47@gmail.com>
2024-06-18 12:13:07 +0100

    update some things

--- app/main/schemas/__init__.py
+++ app/main/schemas/__init__.py
@@ -2,4 +2,4 @@ from .params import CustomParams
 from .admin_key import AdminKey
 from .msg import Msg, BoolStatus, DataDisplay
 from .base import DataList
-from .user import User, UserCreate, UserUpdate, UserList, UserDetail, UserAuthentication, UserUpdates
+from .user import User, UserCreate, UserUpdate, UserList, UserDetail, UserAuthentication, UserUpdate,UserProfileResponse
